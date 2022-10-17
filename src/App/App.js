2cc9d5b393db18770fc037bb11c95a4b6ddb63f8
Hernani Gonzalez <chernani2003@gmail.com>
2022-10-17 16:46:19 -0500

    Commit Initial

--- src/App/App.js
+++ src/App/App.js
@@ -1,17 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+import '../Style/App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.youtube.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -22,4 +21,5 @@ function App() {
   );
 }
 
+// <img src={logo} className="App-logo" alt="logo" /> 
 export default App;
