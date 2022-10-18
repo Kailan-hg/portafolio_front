aee36d839d7223a85e0e667a125010e09a72f3a1
Hernani Gonzalez <chernani2003@gmail.com>
2022-10-17 20:02:03 -0500

    App Format

--- src/App/App.js
+++ src/App/App.js
@@ -1,22 +1,10 @@
 //import logo from './logo.svg';
-import '../Style/App.css';
+//import '../Style/App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://www.youtube.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        Hello World
     </div>
   );
 }
